5ad053daa170cb460ac26e9b76d8faabe83ac551
Martin Linhard <linmad17@htl-kaindorf.at>
2020-06-12 21:30:47 +0200

    Minor fix regarding custom script

--- src/config.rs
+++ src/config.rs
@@ -1,28 +1,32 @@
 use better_dwm_statusbar::prelude::*;
-use std::include_str;
-use std::time::Duration;
 
 pub fn get_config() -> Vec<ModuleWrapper> {
     vec![
         ModuleWrapper(
             Box::new(CustomScript::new(
                 " {VALUE}",
-                include_str!("/home/martin/scripts/get_song.sh"),
+                "/home/martin/scripts/get_song.sh",
             )),
-            Duration::from_secs(2),
+            PauseBetweenYields::from_secs(2),
         ),
-        ModuleWrapper(Box::new(RAMUsage::new("{VALUE} ")), Duration::from_secs(2)),
+        ModuleWrapper(
+            Box::new(RAMUsage::new("{VALUE} ")),
+            PauseBetweenYields::from_secs(2),
+        ),
         ModuleWrapper(
             Box::new(CustomScript::new(
                 "{VALUE} ",
-                include_str!("/home/martin/dwm_config/get_volume.sh"),
+                "/home/martin/dwm_config/get_volume.sh",
             )),
-            Duration::from_millis(200),
+            PauseBetweenYields::from_millis(200),
         ),
         ModuleWrapper(
             Box::new(Battery::new(["", "", "", "", ""], "{VALUE}")),
-            Duration::from_secs(1),
+            PauseBetweenYields::from_secs(1),
         ),
-        ModuleWrapper(Box::new(Time::new("{VALUE} ")), Duration::from_secs(1)),
+        ModuleWrapper(
+            Box::new(Time::new("{VALUE} ")),
+            PauseBetweenYields::from_secs(1),
+        ),
     ]
 }
