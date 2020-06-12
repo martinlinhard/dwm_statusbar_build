cc74f5d27acb8a8e3c9f53b378a70548d5917702
Martin Linhard <linmad17@htl-kaindorf.at>
2020-06-12 19:41:03 +0200

    New statusbar build :)

--- src/config.rs
+++ src/config.rs
@@ -1,10 +1,28 @@
-use better_dwm_statusbar::bar_modules::*;
-use better_dwm_statusbar::module::ModuleWrapper;
+use better_dwm_statusbar::prelude::*;
+use std::include_str;
 use std::time::Duration;
 
 pub fn get_config() -> Vec<ModuleWrapper> {
-    vec![ModuleWrapper(
-        Box::new(Time::new("{VALUE}")),
-        Duration::from_secs(1),
-    )]
+    vec![
+        ModuleWrapper(
+            Box::new(CustomScript::new(
+                " {VALUE}",
+                include_str!("/home/martin/scripts/get_song.sh"),
+            )),
+            Duration::from_secs(2),
+        ),
+        ModuleWrapper(Box::new(RAMUsage::new("{VALUE} ")), Duration::from_secs(2)),
+        ModuleWrapper(
+            Box::new(CustomScript::new(
+                "{VALUE} ",
+                include_str!("/home/martin/dwm_config/get_volume.sh"),
+            )),
+            Duration::from_millis(200),
+        ),
+        ModuleWrapper(
+            Box::new(Battery::new(["", "", "", "", ""], "{VALUE}")),
+            Duration::from_secs(1),
+        ),
+        ModuleWrapper(Box::new(Time::new("{VALUE} ")), Duration::from_secs(1)),
+    ]
 }
--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,4 @@
 mod config;
-use actix::prelude::*;
-use better_dwm_statusbar::module_worker::ModuleWorker;
-use better_dwm_statusbar::status_server::StatusServer;
+use better_dwm_statusbar::start_bar;
 
-use config::get_config;
-
-fn main() {
-    let config = get_config();
-    let server = StatusServer::new(config.len(), " | ");
-
-    System::run(|| {
-        let server_addr = server.start();
-        config
-            .into_iter()
-            .enumerate()
-            .for_each(|(index, module_wrapper)| {
-                let worker = ModuleWorker::new(module_wrapper, server_addr.clone(), index);
-                SyncArbiter::start(1, || worker);
-            });
-    });
-}
+start_bar!(config::get_config());
